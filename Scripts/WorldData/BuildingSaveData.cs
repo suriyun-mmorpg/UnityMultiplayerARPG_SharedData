f32faca7a2c019b0fc70d59f39b6f686d1128eb2
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-02-16 13:32:08 +0700

    feat: gradually move world saves data codes to shared data repo

---
+++ Scripts/WorldData/BuildingSaveData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace MultiplayerARPG
+{
+    public partial struct BuildingSaveData : IBuildingSaveData
+    {
+        public string Id { get; set; }
+
+        public string ParentId { get; set; }
+
+        public int EntityId { get; set; }
+
+        public int CurrentHp { get; set; }
+
+        public float RemainsLifeTime { get; set; }
+
+        public bool IsLocked { get; set; }
+
+        public string LockPassword { get; set; }
+
+        public Vector3 Position { get; set; }
+
+        public Quaternion Rotation { get; set; }
+
+        public string CreatorId { get; set; }
+
+        public string CreatorName { get; set; }
+
+        public string ExtraData { get; set; }
+    }
+}
