9d83b5093f4cc5c7a154047de0774393c4ebf13c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-02-25 13:37:14 +0700

    feat: move item socket's read/write function to shared data repo

--- Scripts/CharacterData/RelatesData/CharacterItem.cs
+++ Scripts/CharacterData/RelatesData/CharacterItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Cysharp.Text;
+using System.Collections.Generic;
 
 namespace MultiplayerARPG
 {
@@ -18,5 +19,41 @@ namespace MultiplayerARPG
         public int randomSeed;
         public int ammo;
         public List<int> sockets = new List<int>();
+
+        public List<int> Sockets
+        {
+            get
+            {
+                if (sockets == null)
+                    sockets = new List<int>();
+                return sockets;
+            }
+        }
+
+        public List<int> ReadSockets(string sockets, char separator = ';')
+        {
+            Sockets.Clear();
+            string[] splitTexts = sockets.Split(separator);
+            foreach (string text in splitTexts)
+            {
+                if (string.IsNullOrEmpty(text))
+                    continue;
+                Sockets.Add(int.Parse(text));
+            }
+            return Sockets;
+        }
+
+        public string WriteSockets(char separator = ';')
+        {
+            using (Utf16ValueStringBuilder stringBuilder = ZString.CreateStringBuilder(true))
+            {
+                foreach (int socket in Sockets)
+                {
+                    stringBuilder.Append(socket);
+                    stringBuilder.Append(separator);
+                }
+                return stringBuilder.ToString();
+            }
+        }
     }
 }
