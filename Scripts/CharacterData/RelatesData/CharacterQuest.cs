d9eb7eb9099de4aab9b21482b98cfd9226726d82
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-02-25 13:40:28 +0700

    feat: move quest task's read/write function to shared data repo

--- Scripts/CharacterData/RelatesData/CharacterQuest.cs
+++ Scripts/CharacterData/RelatesData/CharacterQuest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Cysharp.Text;
+using System.Collections.Generic;
 
 namespace MultiplayerARPG
 {
@@ -11,5 +12,82 @@ namespace MultiplayerARPG
         public bool isTracking;
         public Dictionary<int, int> killedMonsters = new Dictionary<int, int>();
         public List<int> completedTasks = new List<int>();
+
+        public Dictionary<int, int> KilledMonsters
+        {
+            get
+            {
+                if (killedMonsters == null)
+                    killedMonsters = new Dictionary<int, int>();
+                return killedMonsters;
+            }
+        }
+
+        public List<int> CompletedTasks
+        {
+            get
+            {
+                if (completedTasks == null)
+                    completedTasks = new List<int>();
+                return completedTasks;
+            }
+        }
+
+        public Dictionary<int, int> ReadKilledMonsters(string killMonsters)
+        {
+            KilledMonsters.Clear();
+            string[] splitSets = killMonsters.Split(';');
+            foreach (string set in splitSets)
+            {
+                if (string.IsNullOrEmpty(set))
+                    continue;
+                string[] splitData = set.Split(':');
+                if (splitData.Length != 2)
+                    continue;
+                KilledMonsters[int.Parse(splitData[0])] = int.Parse(splitData[1]);
+            }
+            return KilledMonsters;
+        }
+
+        public string WriteKilledMonsters()
+        {
+            using (Utf16ValueStringBuilder stringBuilder = ZString.CreateStringBuilder(true))
+            {
+                foreach (KeyValuePair<int, int> keyValue in KilledMonsters)
+                {
+                    stringBuilder.Append(keyValue.Key);
+                    stringBuilder.Append(':');
+                    stringBuilder.Append(keyValue.Value);
+                    stringBuilder.Append(';');
+                }
+                return stringBuilder.ToString();
+            }
+        }
+
+        public List<int> ReadCompletedTasks(string completedTasks)
+        {
+            CompletedTasks.Clear();
+            string[] splitTexts = completedTasks.Split(';');
+            foreach (string text in splitTexts)
+            {
+                if (string.IsNullOrEmpty(text))
+                    continue;
+                CompletedTasks.Add(int.Parse(text));
+            }
+            return CompletedTasks;
+        }
+
+        public string WriteCompletedTasks()
+        {
+            using (Utf16ValueStringBuilder stringBuilder = ZString.CreateStringBuilder(true))
+            {
+                foreach (int completedTask in CompletedTasks)
+                {
+                    stringBuilder.Append(completedTask);
+                    stringBuilder.Append(';');
+                }
+                return stringBuilder.ToString();
+            }
+        }
     }
 }
