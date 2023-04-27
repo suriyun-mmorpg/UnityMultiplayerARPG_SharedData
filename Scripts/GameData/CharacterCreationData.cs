13ace254573fe8a1bf773c9e2a5ed0b6d256acf4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-28 04:41:11 +0700

    feat: add function to set create character data

--- Scripts/GameData/CharacterCreationData.cs
+++ Scripts/GameData/CharacterCreationData.cs
@@ -8,19 +8,28 @@ namespace MultiplayerARPG
         public Dictionary<int, Dictionary<int, PlayerCharacterData>> AvailableCharacters { get; private set; } = new Dictionary<int, Dictionary<int, PlayerCharacterData>>();
         public List<int> AvailableFactionIds { get; private set; } = new List<int>();
 
-        public bool CanCreateCharacter(int entityId, int dataId, int factionId)
+        public bool CanCreateCharacter(int dataId, int entityId, int factionId)
         {
             return AvailableCharacters.ContainsKey(entityId) && AvailableCharacters[entityId].ContainsKey(dataId) && AvailableFactionIds.Contains(factionId);
         }
 
-        public PlayerCharacterData GetCreateCharacterData(string id, string userId, string characterName, int entity, int dataId, int factionId)
+        public PlayerCharacterData GetCreateCharacterData(string id, string userId, string characterName, int dataId, int entityId, int factionId)
         {
-            PlayerCharacterData result = AvailableCharacters[entity][dataId].CloneTo(new PlayerCharacterData());
+            PlayerCharacterData result = AvailableCharacters[entityId][dataId].CloneTo(new PlayerCharacterData());
             result.Id = id;
             result.UserId = userId;
             result.CharacterName = characterName;
             result.FactionId = factionId;
             return result;
+        }
+
+        public void SetCreateCharacterData(PlayerCharacterData data, string id, string userId, string characterName, int dataId, int entityId, int factionId)
+        {
+            data = AvailableCharacters[entityId][dataId].CloneTo(data);
+            data.Id = id;
+            data.UserId = userId;
+            data.CharacterName = characterName;
+            data.FactionId = factionId;
         }
     }
 }
