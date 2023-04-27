a16d67010f72326bde98ee60a20650a8f470b232
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-28 01:22:44 +0700

    feat: add Id, UserId to `GetCreateCharacterData function

--- Scripts/GameData/CharacterCreationData.cs
+++ Scripts/GameData/CharacterCreationData.cs
@@ -5,17 +5,19 @@ namespace MultiplayerARPG
     [System.Serializable]
     public partial class CharacterCreationData
     {
-        public Dictionary<int, Dictionary<int, PlayerCharacterData>> AvailableCharacters = new Dictionary<int, Dictionary<int, PlayerCharacterData>>();
-        public HashSet<int> AvailableFactionIds = new HashSet<int>();
+        public Dictionary<int, Dictionary<int, PlayerCharacterData>> AvailableCharacters { get; private set; } = new Dictionary<int, Dictionary<int, PlayerCharacterData>>();
+        public List<int> AvailableFactionIds { get; private set; } = new List<int>();
 
         public bool CanCreateCharacter(int entityId, int dataId, int factionId)
         {
             return AvailableCharacters.ContainsKey(entityId) && AvailableCharacters[entityId].ContainsKey(dataId) && AvailableFactionIds.Contains(factionId);
         }
 
-        public PlayerCharacterData GetCreateCharacterData(string characterName, int entity, int dataId, int factionId)
+        public PlayerCharacterData GetCreateCharacterData(string id, string userId, string characterName, int entity, int dataId, int factionId)
         {
             PlayerCharacterData result = AvailableCharacters[entity][dataId].CloneTo(new PlayerCharacterData());
+            result.Id = id;
+            result.UserId = userId;
             result.CharacterName = characterName;
             result.FactionId = factionId;
             return result;
