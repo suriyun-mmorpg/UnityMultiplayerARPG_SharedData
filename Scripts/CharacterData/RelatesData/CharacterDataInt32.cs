f163b5957d57b7ba50e0ecbf36f8deb331b1780c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-06-20 13:23:46 +0700

    feat: add create and clone functions to custom data

--- Scripts/CharacterData/RelatesData/CharacterDataInt32.cs
+++ Scripts/CharacterData/RelatesData/CharacterDataInt32.cs
@@ -5,5 +5,23 @@ namespace MultiplayerARPG
     {
         public int hashedKey;
         public int value;
+
+        public CharacterDataInt32 Clone()
+        {
+            return new CharacterDataInt32()
+            {
+                hashedKey = hashedKey,
+                value = value,
+            };
+        }
+
+        public static CharacterDataInt32 Create(int hashedKey, int value = 0)
+        {
+            return new CharacterDataInt32()
+            {
+                hashedKey = hashedKey,
+                value = value,
+            };
+        }
     }
 }
