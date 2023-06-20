98ca38f77c661c3594a12b6e3438da75e1ab9627
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-06-20 12:56:17 +0700

    feat: add custom data
    # Conflicts:
    #	Scripts/CharacterData/PlayerCharacterData.cs

--- Scripts/CharacterData/PlayerCharacterData.cs
+++ Scripts/CharacterData/PlayerCharacterData.cs
@@ -8,6 +8,15 @@ namespace MultiplayerARPG
         private List<CharacterHotkey> _hotkeys = new List<CharacterHotkey>();
         private List<CharacterQuest> _quests = new List<CharacterQuest>();
         private List<CharacterCurrency> _currencies = new List<CharacterCurrency>();
+        private List<CharacterDataBoolean> _serverBools = new List<CharacterDataBoolean>();
+        private List<CharacterDataInt32> _serverInts = new List<CharacterDataInt32>();
+        private List<CharacterDataFloat32> _serverFloats = new List<CharacterDataFloat32>();
+        private List<CharacterDataBoolean> _privateBools = new List<CharacterDataBoolean>();
+        private List<CharacterDataInt32> _privateInts = new List<CharacterDataInt32>();
+        private List<CharacterDataFloat32> _privateFloats = new List<CharacterDataFloat32>();
+        private List<CharacterDataBoolean> _publicBools = new List<CharacterDataBoolean>();
+        private List<CharacterDataInt32> _publicInts = new List<CharacterDataInt32>();
+        private List<CharacterDataFloat32> _publicFloats = new List<CharacterDataFloat32>();
 
         public string UserId { get; set; }
         public int FactionId { get; set; }
@@ -57,6 +66,96 @@ namespace MultiplayerARPG
             {
                 _currencies = new List<CharacterCurrency>();
                 _currencies.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataBoolean> ServerBools
+        {
+            get { return _serverBools; }
+            set
+            {
+                _serverBools = new List<CharacterDataBoolean>();
+                _serverBools.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataInt32> ServerInts
+        {
+            get { return _serverInts; }
+            set
+            {
+                _serverInts = new List<CharacterDataInt32>();
+                _serverInts.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataFloat32> ServerFloats
+        {
+            get { return _serverFloats; }
+            set
+            {
+                _serverFloats = new List<CharacterDataFloat32>();
+                _serverFloats.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataBoolean> PrivateBools
+        {
+            get { return _privateBools; }
+            set
+            {
+                _privateBools = new List<CharacterDataBoolean>();
+                _privateBools.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataInt32> PrivateInts
+        {
+            get { return _privateInts; }
+            set
+            {
+                _privateInts = new List<CharacterDataInt32>();
+                _privateInts.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataFloat32> PrivateFloats
+        {
+            get { return _privateFloats; }
+            set
+            {
+                _privateFloats = new List<CharacterDataFloat32>();
+                _privateFloats.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataBoolean> PublicBools
+        {
+            get { return _publicBools; }
+            set
+            {
+                _publicBools = new List<CharacterDataBoolean>();
+                _publicBools.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataInt32> PublicInts
+        {
+            get { return _publicInts; }
+            set
+            {
+                _publicInts = new List<CharacterDataInt32>();
+                _publicInts.AddRange(value);
+            }
+        }
+
+        public IList<CharacterDataFloat32> PublicFloats
+        {
+            get { return _publicFloats; }
+            set
+            {
+                _publicFloats = new List<CharacterDataFloat32>();
+                _publicFloats.AddRange(value);
             }
         }
     }
---
+++ Scripts/CharacterData/RelatesData/CharacterDataInt32.cs
@@ -0,0 +1,9 @@
+namespace MultiplayerARPG
+{
+    [System.Serializable]
+    public class CharacterDataInt32
+    {
+        public int hashedKey;
+        public int value;
+    }
+}
