3e966cbd5dc4a06ec35f3e18a20e81370b133885
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-07-04 02:27:22 +0700

    feat: add formatted game message

---
+++ Scripts/Networking/Messages/Messages/FormattedGameMessage.cs
@@ -0,0 +1,23 @@
+﻿using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
+{
+    [System.Serializable]
+    public partial struct FormattedGameMessage : INetSerializable
+    {
+        public UIFormatKeys format;
+        public string[] args;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            format = (UIFormatKeys)reader.GetPackedUShort();
+            args = reader.GetArray<string>();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.PutPackedUShort((ushort)format);
+            writer.PutArray<string>(args);
+        }
+    }
+}
