5fe09ac307e02eb362b44ffd4c3715d18ef14141
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-08-24 10:27:27 +0700

    feat: generate new Id for relates data when creating new character

--- Scripts/GameData/CharacterCreationData.cs
+++ Scripts/GameData/CharacterCreationData.cs
@@ -28,7 +28,7 @@ namespace MultiplayerARPG
 
         public void SetCreateCharacterData(PlayerCharacterData data, string id, string userId, string characterName, int dataId, int entityId, int factionId, IList<CharacterDataBoolean> publicBools, IList<CharacterDataInt32> publicInts, IList<CharacterDataFloat32> publicFloats)
         {
-            data = AvailableCharacters[entityId][dataId].CloneTo(data);
+            data = AvailableCharacters[entityId][dataId].CloneTo(data, generateNewIdForRelatesData: true);
             data.Id = id;
             data.UserId = userId;
             data.CharacterName = characterName;
