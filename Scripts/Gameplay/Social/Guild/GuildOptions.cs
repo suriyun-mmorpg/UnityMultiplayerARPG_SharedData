3157cb6b1998c769bede51c387721c8d20fafbcb
insthync <ittipon.bay@gmail.com>
2024-04-03 13:48:54 +0700

    feat: move guild list entry, guild list field options, guild options to be part of shared data

---
+++ Scripts/Gameplay/Social/Guild/GuildOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MultiplayerARPG
+{
+    /// <summary>
+    /// NOTE: Make this struct to be partial struct in-case other developer wants to add other options
+    /// </summary>
+    [System.Serializable]
+    [StructLayout(LayoutKind.Auto)]
+    public partial struct GuildOptions
+    {
+        public int iconDataId;
+    }
+}
