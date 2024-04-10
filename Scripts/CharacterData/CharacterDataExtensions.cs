9eb3440316a4703a9746da4db3eb1c7c429b8ae5
insthync <ittipon.bay@gmail.com>
2024-04-10 11:13:42 +0700

    feat: move fill weapon sets if needed function to shared data

--- Scripts/CharacterData/CharacterDataExtensions.cs
+++ Scripts/CharacterData/CharacterDataExtensions.cs
@@ -1,3 +1,4 @@
+using LiteNetLibManager;
 using System.Collections.Generic;
 
 namespace MultiplayerARPG
@@ -69,6 +70,21 @@ namespace MultiplayerARPG
         public static int IndexOfSummon(this ICharacterData data, SummonType type, int dataId)
         {
             return data.Summons.IndexOf(type, dataId);
+        }
+
+        public static void FillWeaponSetsIfNeeded(this ICharacterData data, byte equipWeaponSet)
+        {
+#if UNITY_2017_1_OR_NEWER
+            if (data is IGameEntity gameEntity && gameEntity.Entity.Manager != null && !gameEntity.Entity.IsServer)
+            {
+                Logging.LogWarning("[FillWeaponSetsIfNeeded] Client can't fill weapon sets");
+                return;
+            }
+#endif
+            while (data.SelectableWeaponSets.Count <= equipWeaponSet)
+            {
+                data.SelectableWeaponSets.Add(new EquipWeapons());
+            }
         }
     }
 }
