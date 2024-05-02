b4771c06ee14cf3f99e43d7e240c0fc4353f59ac
insthync <ittipon.bay@gmail.com>
2024-05-02 17:55:32 +0700

    feat: move user data to be part of shared data

---
+++ Scripts/UserData/UnlockableContent.cs
@@ -0,0 +1,29 @@
+using LiteNetLib.Utils;
+
+namespace MultiplayerARPG
+{
+    [System.Serializable]
+    public struct UnlockableContent : INetSerializable
+    {
+        public UnlockableContentType type;
+        public int dataId;
+        public int progression;
+        public bool unlocked;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            type = (UnlockableContentType)reader.GetByte();
+            dataId = reader.GetPackedInt();
+            progression = reader.GetPackedInt();
+            unlocked = reader.GetBool();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put((byte)type);
+            writer.PutPackedInt(dataId);
+            writer.PutPackedInt(progression);
+            writer.Put(unlocked);
+        }
+    }
+}
