0dbaba84f1c65a3738715c598818e35ed7950f3c
insthync <ittipon.bay@gmail.com>
2024-09-28 02:08:30 +0700

    feat: add storage data

--- Scripts/Storage/StorageCharacterItem.cs
+++ Scripts/Storage/StorageCharacterItem.cs
@@ -54,6 +54,33 @@ namespace MultiplayerARPG
         }
     }
 
+    public class UserUsingStorageData
+    {
+        public StorageId Id { get; set; }
+        public bool RequireEntity { get; set; }
+        public IActivatableEntity Entity { get; set; }
+
+        public string GetId()
+        {
+            return Id.GetId();
+        }
+
+        public override int GetHashCode()
+        {
+            return GetId().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetId();
+        }
+    }
+
     public struct StorageItemId
     {
         public static readonly StorageItemId Empty = new StorageItemId(StorageType.None, string.Empty, -1);
