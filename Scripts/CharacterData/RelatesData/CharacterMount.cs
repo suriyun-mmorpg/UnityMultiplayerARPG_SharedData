b7fd2a5c109f4f56b23c8c8b35a9c23b0e34d658
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-10-09 01:16:03 +0700

    feat: WIP new mount structure (net serializing)

--- Scripts/CharacterData/RelatesData/CharacterMount.cs
+++ Scripts/CharacterData/RelatesData/CharacterMount.cs
@@ -1,7 +1,9 @@
+using LiteNetLib.Utils;
+
 namespace MultiplayerARPG
 {
     [System.Serializable]
-    public partial struct CharacterMount
+    public partial struct CharacterMount : INetSerializable
     {
         public byte mountType;
         public int dataId;
@@ -9,5 +11,27 @@ namespace MultiplayerARPG
         public int exp;
         public bool isMounted;
         public long unmountTime;
+
+        public void Deserialize(NetDataReader reader)
+        {
+            mountType = reader.GetByte();
+            dataId = reader.GetPackedInt();
+            level = reader.GetPackedInt();
+            exp = reader.GetPackedInt();
+            isMounted = reader.GetBool();
+            if (isMounted)
+                unmountTime = reader.GetPackedLong();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(mountType);
+            writer.PutPackedInt(dataId);
+            writer.PutPackedInt(level);
+            writer.PutPackedInt(exp);
+            writer.Put(isMounted);
+            if (isMounted)
+                writer.PutPackedLong(unmountTime);
+        }
     }
 }
