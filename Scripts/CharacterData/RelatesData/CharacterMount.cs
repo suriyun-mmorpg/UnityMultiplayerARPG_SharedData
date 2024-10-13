9265204f050a13ca19eebaf4bd3ddb42dc52fdfc
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-10-13 17:15:28 +0700

    feat: add clone and create functions

--- Scripts/CharacterData/RelatesData/CharacterMount.cs
+++ Scripts/CharacterData/RelatesData/CharacterMount.cs
@@ -15,5 +15,28 @@ namespace MultiplayerARPG
         public int dataId;
         public int level;
         public int exp;
+
+        public CharacterMount Clone()
+        {
+            CharacterMount result = new CharacterMount()
+            {
+                type = type,
+                dataId = dataId,
+                level = level,
+                exp = exp,
+            };
+            return result;
+        }
+
+        public static CharacterMount Create(MountType type, int dataId, int level = 1, int exp = 0)
+        {
+            return new CharacterMount()
+            {
+                type = type,
+                dataId = dataId,
+                level = level,
+                exp = exp,
+            };
+        }
     }
 }
