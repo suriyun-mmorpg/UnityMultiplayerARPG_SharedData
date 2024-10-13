f2a030697a3bf8f9eaaab14230983c38272fb16d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-10-14 02:53:36 +0700

    feat: character mount structure changes

--- Scripts/CharacterData/RelatesData/CharacterMount.cs
+++ Scripts/CharacterData/RelatesData/CharacterMount.cs
@@ -15,11 +15,8 @@ namespace MultiplayerARPG
         public MountType type;
         public int dataId;
         public float mountRemainsDuration;
-        public uint objectId;
         public int level;
-        public int exp;
         public int currentHp;
-        public int currentMp;
 
         public CharacterMount Clone()
         {
@@ -28,22 +25,10 @@ namespace MultiplayerARPG
                 type = type,
                 dataId = dataId,
                 mountRemainsDuration = mountRemainsDuration,
-                objectId = objectId,
                 level = level,
-                exp = exp,
                 currentHp = currentHp,
-                currentMp = currentMp,
             };
             return result;
-        }
-
-        public static CharacterMount Create(MountType type, int dataId)
-        {
-            return new CharacterMount()
-            {
-                type = type,
-                dataId = dataId,
-            };
         }
     }
 }
