3d1be528b48bd1bbad5ae4ea3757de6802eb04e7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-02-27 17:26:19 +0700

    feat: improve item using codes, return true/false to determine that the item being used properly or not

--- Scripts/UIMessages/DefaultLocale.cs
+++ Scripts/UIMessages/DefaultLocale.cs
@@ -699,6 +699,7 @@ namespace MultiplayerARPG
             // Character Name Changing
             Texts.Add(UITextKeys.UI_CHARACTER_NAME_CHANGE.ToString(), "Character Name Changing");
             Texts.Add(UITextKeys.UI_CHARACTER_NAME_CHANGE_DESCRIPTION.ToString(), "Which name do you want to make changes?");
+            Texts.Add(UITextKeys.UI_CHARACTER_NAME_CHANGE_SUCCESS.ToString(), "Character name change success");
         }
     }
 }
