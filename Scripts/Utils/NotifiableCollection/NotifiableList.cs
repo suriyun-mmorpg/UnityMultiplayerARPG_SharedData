97c7cfcd9c68b203ac6d6174c9e194744bb3f74b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-07-17 13:40:35 +0700

    feat: add lock object to notifiable list

--- Scripts/Utils/NotifiableCollection/NotifiableList.cs
+++ Scripts/Utils/NotifiableCollection/NotifiableList.cs
@@ -8,6 +8,7 @@ namespace NotifiableCollection
         public delegate void OnChangedDelegate(NotifiableListAction action, int index, TType oldItem, TType newItem);
         public delegate void OnChangedWithoutItemDelegate(NotifiableListAction action, int index);
         protected readonly List<TType> _list;
+        private readonly object _lockObject = new object();
 
         public event OnChangedDelegate ListChanged;
         public event OnChangedWithoutItemDelegate ListChangedWithoutItem;
@@ -29,12 +30,21 @@ namespace NotifiableCollection
 
         public TType this[int index]
         {
-            get { return _list[index]; }
+            get
+            {
+                lock (_lockObject)
+                {
+                    return _list[index];
+                }
+            }
             set
             {
-                TType oldItem = _list[index];
-                _list[index] = value;
-                InvokeNotifiableListAction(NotifiableListAction.Set, index, oldItem, value);
+                lock (_lockObject)
+                {
+                    TType oldItem = _list[index];
+                    _list[index] = value;
+                    InvokeNotifiableListAction(NotifiableListAction.Set, index, oldItem, value);
+                }
             }
         }
 
@@ -44,85 +54,115 @@ namespace NotifiableCollection
 
         public void Add(TType item)
         {
-            int index = _list.Count;
-            _list.Add(item);
-            InvokeNotifiableListAction(NotifiableListAction.Add, index, default, item);
+            lock (_lockObject)
+            {
+                int index = _list.Count;
+                _list.Add(item);
+                InvokeNotifiableListAction(NotifiableListAction.Add, index, default, item);
+            }
         }
 
         public void AddRange(IEnumerable<TType> collection)
         {
-            foreach (TType item in collection)
+            lock (_lockObject)
             {
-                Add(item);
+                foreach (TType item in collection)
+                {
+                    Add(item);
+                }
             }
         }
 
         public void Clear()
         {
-            _list.Clear();
-            InvokeNotifiableListAction(NotifiableListAction.Clear, -1, default, default);
+            lock (_lockObject)
+            {
+                _list.Clear();
+                InvokeNotifiableListAction(NotifiableListAction.Clear, -1, default, default);
+            }
         }
 
         public bool Contains(TType item)
         {
-            return _list.Contains(item);
+            lock (_lockObject)
+            {
+                return _list.Contains(item);
+            }
         }
 
         public void CopyTo(TType[] array, int arrayIndex)
         {
-            _list.CopyTo(array, arrayIndex);
+            lock (_lockObject)
+            {
+                _list.CopyTo(array, arrayIndex);
+            }
         }
 
         public IEnumerator<TType> GetEnumerator()
         {
-            return _list.GetEnumerator();
-        }
-
-        public int IndexOf(TType item)
-        {
-            return _list.IndexOf(item);
-        }
-
-        public void Insert(int index, TType item)
-        {
-            _list.Insert(index, item);
-            InvokeNotifiableListAction(NotifiableListAction.Insert, index, default, item);
-        }
-
-        public bool Remove(TType item)
-        {
-            int index = IndexOf(item);
-            if (index >= 0)
+            lock (_lockObject)
             {
-                RemoveAt(index);
-                return true;
-            }
-            return false;
-        }
-
-        public void RemoveAt(int index)
-        {
-            TType oldItem = _list[index];
-            if (index == 0)
-            {
-                _list.RemoveAt(index);
-                InvokeNotifiableListAction(NotifiableListAction.RemoveFirst, index, oldItem, default);
-            }
-            else if (index == _list.Count - 1)
-            {
-                _list.RemoveAt(index);
-                InvokeNotifiableListAction(NotifiableListAction.RemoveLast, index, oldItem, default);
-            }
-            else
-            {
-                _list.RemoveAt(index);
-                InvokeNotifiableListAction(NotifiableListAction.RemoveAt, index, oldItem, default);
+                return _list.GetEnumerator();
             }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _list.GetEnumerator();
+            return GetEnumerator();
+        }
+
+        public int IndexOf(TType item)
+        {
+            lock (_lockObject)
+            {
+                return _list.IndexOf(item);
+            }
+        }
+
+        public void Insert(int index, TType item)
+        {
+            lock (_lockObject)
+            {
+                _list.Insert(index, item);
+                InvokeNotifiableListAction(NotifiableListAction.Insert, index, default, item);
+            }
+        }
+
+        public bool Remove(TType item)
+        {
+            lock (_lockObject)
+            {
+                int index = IndexOf(item);
+                if (index >= 0)
+                {
+                    RemoveAt(index);
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            lock (_lockObject)
+            {
+                TType oldItem = _list[index];
+                if (index == 0)
+                {
+                    _list.RemoveAt(index);
+                    InvokeNotifiableListAction(NotifiableListAction.RemoveFirst, index, oldItem, default);
+                }
+                else if (index == _list.Count - 1)
+                {
+                    _list.RemoveAt(index);
+                    InvokeNotifiableListAction(NotifiableListAction.RemoveLast, index, oldItem, default);
+                }
+                else
+                {
+                    _list.RemoveAt(index);
+                    InvokeNotifiableListAction(NotifiableListAction.RemoveAt, index, oldItem, default);
+                }
+            }
         }
 
         public void Dirty(int index)
